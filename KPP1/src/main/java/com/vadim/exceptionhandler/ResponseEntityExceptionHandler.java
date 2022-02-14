cbba0cba7da2dcf86b69b21cf57200dec8544721
= <=>
2022-02-14 18:22:48 +0300

    Added exception handling

--- KPP1/src/main/java/com/vadim/exceptionhandler/ResponseEntityExceptionHandler.java
+++ KPP1/src/main/java/com/vadim/exceptionhandler/ResponseEntityExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.vadim.exceptionhandler;
 
+import com.vadim.exception.CalculatingException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,6 +15,12 @@ public class ResponseEntityExceptionHandler {
     public ResponseEntity<?> handleAuthenticationException(ConstraintViolationException e) {
         ExceptionInfo info = new ExceptionInfo(e.getMessage(), HttpStatus.BAD_REQUEST.value());
         return new ResponseEntity<>(info, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(CalculatingException.class)
+    public ResponseEntity<?> handleCalculatingException(CalculatingException e) {
+        ExceptionInfo info = new ExceptionInfo(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return new ResponseEntity<>(info, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
