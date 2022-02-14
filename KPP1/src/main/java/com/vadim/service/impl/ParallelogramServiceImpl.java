9e4aa3eda65994ff39adc13de27001448de8091c
= <=>
2022-02-14 18:30:34 +0300

    Refactoring

--- KPP1/src/main/java/com/vadim/service/impl/ParallelogramServiceImpl.java
+++ KPP1/src/main/java/com/vadim/service/impl/ParallelogramServiceImpl.java
@@ -1,6 +1,7 @@
 package com.vadim.service.impl;
 
 import com.vadim.dto.ParallelogramDto;
+import com.vadim.exception.CalculatingException;
 import com.vadim.service.ParallelogramService;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,10 @@ public class ParallelogramServiceImpl implements ParallelogramService {
 
     @Override
     public ParallelogramDto getParallelogram(Double width, Double height) {
-       return new ParallelogramDto(width, height);
+        if (width * height > 100000D) {
+            throw new CalculatingException("This is an exception just to show that it works");
+        }
+        return new ParallelogramDto(width, height);
     }
+
 }
